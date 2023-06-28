103915be07264ba3003d0a962695e858a615de16
mwmcclure7 <106849204+mwmcclure7@users.noreply.github.com>
2023-06-28 19:42:52 -0400

    Fresh start

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,7 +5,6 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 
 public class RobotContainer {
   public RobotContainer() {
@@ -15,6 +14,6 @@ public class RobotContainer {
   private void configureBindings() {}
 
   public Command getAutonomousCommand() {
-    
+    return null;
   }
 }
